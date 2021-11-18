51e76075cfd167b675b753a5662b048b432fa670
shabrat <94321779+shabrat@users.noreply.github.com>
2021-11-18 17:25:20 +0200

    Create task2.py

---
+++ HT_1/task2.py
@@ -0,0 +1,4 @@
+color_list_1 = set(["White", "Black", "Red"])
+color_list_2 = set(["Red", "Green"])
+print("\nColours from color_list_1 which are not present in color_list_2:")
+print(color_list_1.difference(color_list_2))
