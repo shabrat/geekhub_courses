b28f130f4e541ae0cd31712b01d2d8742eb62ea2
shabrat <94321779+shabrat@users.noreply.github.com>
2021-11-18 22:50:21 +0200

    Update task4.py
    Write a script to concatenate N strings

--- HT_1/task4.py
+++ HT_1/task4.py
@@ -1,7 +1,7 @@
-var1 = "Hello "
-var2 = "mentors! "
-var3 = "Have "
-var4 = "a nice "
-var5 = "day!"
-var6 = var1 + var2 + var3 + var4 + var5
-print(var6)
+any_number = int(input("Ener any number "))
+result = ""
+for a in range(any_number):
+    print(a)
+    user_input = input("Output")
+    result = result + user_input
+print(result)
