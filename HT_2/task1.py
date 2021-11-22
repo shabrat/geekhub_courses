efa31c7401fea7580ee3b5c99151e7f7bb0fff0d
shabrat <94321779+shabrat@users.noreply.github.com>
2021-11-22 15:20:02 +0200

    Create task1.py
    1. Написати скрипт, який конкатенує всі елементи в списку і виведе їх на екран. Список можна

---
+++ HT_2/task1.py
@@ -0,0 +1,9 @@
+def concatenate_list_data(list):
+    result= ''
+    for element in list:
+        result += str(element)
+    return result
+    for element in list:
+        result += int(element)
+
+print(concatenate_list_data([1, 9, 9, 6, "Hi Mentor"],), 1, 9,9,6)
