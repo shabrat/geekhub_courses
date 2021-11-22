07ca9b15595eea7661dd840ef3ce589b6ad52cf0
shabrat <94321779+shabrat@users.noreply.github.com>
2021-11-22 18:36:11 +0200

    Create task7.py
    Написати скрипт, який отримає максимальне і мінімальне значення із словника.

---
+++ HT_2/task7.py
@@ -0,0 +1,7 @@
+dict = {'a':1, 'b':-1, 'c': 1996, 'd': 5, 'e': 12}
+
+key_max = max(dict.keys(), key=(lambda k: dict[k]))
+key_min = min(dict.keys(), key=(lambda k: dict[k]))
+
+print('Minimum Value: ',dict[key_min])
+print('Maximum Value: ',dict[key_max])
