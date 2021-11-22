eef52f19b85443721b59e5075164932bf02ceffa
shabrat <94321779+shabrat@users.noreply.github.com>
2021-11-22 18:30:55 +0200

    Create task5.py
    Написати скрипт, який залишить в словнику тільки пари із унікальними значеннями (дублікати значень - видалити).

---
+++ HT_2/task5.py
@@ -0,0 +1,29 @@
+student = {'id1':
+   {'name': ['Artur'],
+    'class': ['11-A'],
+    'subjects': ['english, history, biology']
+   },
+ 'id2':
+  {'name': ['Andrew'],
+    'class': ['9-A'],
+    'subjects': ['english, history, biology']
+   },
+ 'id3':
+    {'name': ['Artur'],
+    'class': ['11-A'],
+    'subjects': ['english, history, biology']
+   },
+ 'id4':
+   {'name': ['Artem'],
+    'class': ['11-B'],
+    'subjects': ['english, history, biology']
+   },
+}
+result = {}
+
+for key,value in student.items():
+
+    if value not in result.values():
+        result[key] = value
+
+print(result)
